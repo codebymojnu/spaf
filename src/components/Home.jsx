7bd77d3d6e8de3544f5dabadc65832c6651b3e3f
codebymojnu <thisismojnu@gmail.com>
2024-03-06 22:18:47 +0600

    react markdown news added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,18 @@
+import { useEffect, useState } from "react";
+import newsData from "../newsData/newsData.json";
 import DonateForm from "./DonateForm";
 import HeroSection from "./HeroSection";
 import NewsTicker from "./NewsTricker";
 
 export default function Home() {
+  const [loadNews, setLoadNews] = useState([]);
+
+  useEffect(() => {
+    setLoadNews(newsData);
+  }, []);
   return (
     <>
-      <NewsTicker />
+      <NewsTicker newsData={loadNews} />
       <DonateForm />
       <HeroSection />
     </>
--- src/components/NewsTricker.jsx
+++ src/components/NewsTricker.jsx
@@ -1,14 +1,50 @@
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
 import "./news-ticker.css";
 
-function NewsTicker() {
+function NewsTicker({ newsData }) {
+  const [selectedNews, setSelectedNews] = useState(null);
+
+  const handleNewsClick = (news) => {
+    setSelectedNews(news);
+  };
+
+  const closeModal = () => {
+    setSelectedNews(null);
+  };
+
   return (
-    <div className="news-ticker">
+    <div className="flex news-ticker">
+      {/* Special News Animation */}
+      <div className="special-news-animation">বিশেষ সংবাদ</div>
+
       <div className="ticker-content">
-        <div className="ticker-text">News 1</div>
-        <div className="ticker-text">News 2</div>
-        <div className="ticker-text">News 3</div>
-        {/* Add more news items as needed */}
+        {newsData.map((news, index) => (
+          <div
+            className="ticker-text"
+            key={index}
+            onClick={() => handleNewsClick(news)}
+          >
+            {news.title}
+          </div>
+        ))}
       </div>
+      {selectedNews && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <h2>{selectedNews.title}</h2>
+            <div className="markdown-container">
+              <ReactMarkdown>{selectedNews.markdownTextNews}</ReactMarkdown>
+            </div>
+            <div className="flex justify-center">
+              <img src={selectedNews?.image} alt={selectedNews.title} />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
